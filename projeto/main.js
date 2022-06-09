eaaf86594a120122b6d398e92214b4e4c012a5b3
Thiago Costa <thiagomoreiracosta@gmail.com>
2022-06-08 23:52:52 -0300

    finalizado link e carregamento de dados na planilha google

--- projeto/main.js
+++ projeto/main.js
@@ -28,3 +28,32 @@ inputs.forEach((input) => input.addEventListener("focusout", handleFocusOut));
 
 inputs.forEach((input) => input.addEventListener("input", handleChange));
 
+const addLoading = () => {
+  button.innerHTML =
+    '<img src="./img/loading-process-svgrepo-com.svg" class="loading">';
+};
+
+const removeLoading = () => {
+  button.innerHTML = '<img src="./img/iconmonstr-arrow-right-lined.svg">';
+};
+
+// addLoading();
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  addLoading();
+
+  const username = document.querySelector('input[name="username"]').value;
+  const password = document.querySelector('input[name="password"]').value;
+
+  fetch("https://api.sheetmonkey.io/form/fU6NWj6uTX3zL88BUs5nVz", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, password }),
+  }).then(() => removeLoading());
+};
+
+document.querySelector("form").addEventListener("submit", handleSubmit);
